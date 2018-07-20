12a189e8c2587ecbcc1388017776cb12a6db4a14
Joao Victor Machado <jvmachadorj@gmail.com>
2018-07-20 16:48:23 -0300

    changes to run in heroku

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'pareto'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Climate, '/climate/<int:_id>')
